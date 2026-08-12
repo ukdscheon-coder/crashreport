23a6fbc87a47c71d8818ef91ac89f7a7f1fa3a8b
ukdscheon-coder <uk.dscheon@gmail.com>
2026-08-12 01:55:34 +0100

    Workers static assets deploy

---
+++ worker.js
@@ -0,0 +1,6 @@
+﻿export default {
+  async fetch(request, env) {
+    if (env.ASSETS) return env.ASSETS.fetch(request);
+    return new Response('ASSETS binding missing', { status: 500 });
+  },
+};
