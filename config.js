1590ace4fe75467d984d63dbf0fbf10d05dc755a
Daeseong Cheon <uk.dscheon@gmail.com>
2026-08-16 17:46:27 +0100

    Add Stripe test publishable key

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window.CRASHREPORT_CONFIG = {
   beta: true,
   stripe: {
     mode: 'test',
-    publishableKey: '',
+    publishableKey: 'pk_test_51U3QjtIryG8HCIAEfdKnir6yXe9UvR2M9SgLG3yGtBsYXDnKugfdloSKSVapW7AKuqcz4inHvZfCshaqL0cLvQmI00V9ajAfhg',
     paymentLinkMonthly: 'https://buy.stripe.com/test_6oU00jbIwa3xaXL8xW9AA01',
     paymentLinkYearly: 'https://buy.stripe.com/test_4gM9AT6ocejN5DraG49AA00'
   }
