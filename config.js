45b1a13f259c1288ac614155eaf8bd94f7fff139
Daeseong Cheon <uk.dscheon@gmail.com>
2026-08-17 13:34:35 +0100

    Add live one-time Claim Pack checkout

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ window.CRASHREPORT_CONFIG = {
   beta: true,
   stripe: {
     mode: 'live',
+    paymentLinkClaimPack: 'https://buy.stripe.com/cNi6oIcTbbdh12z45s57W04',
     paymentLinkMonthly: 'https://buy.stripe.com/dRm7sM3iBept9z5cBY57W02',
     paymentLinkYearly: 'https://buy.stripe.com/6oUeVe5qJ0yDdPleK657W03'
   }
